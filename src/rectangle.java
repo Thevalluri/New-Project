150f5e4bc4475deea7ade31222e8ccf797c3acdc
root <root@ip-172-31-33-80.us-east-2.compute.internal>
2018-01-26 23:20:43 +0000

    added

---
+++ src/rectangle.java
@@ -0,0 +1,17 @@
+Public class Rectangle {
+ public int length;
+ public int width;
+
+ publuc Rectangle(int length, int width) {
+ this.length = length;
+ this.width = width;
+ }
+ 
+ Public int getArea() {
+  return length * width;
+ }
+
+ public int getPerimeter() {
+  return 2 * (length + breadth);
+ }
+}
